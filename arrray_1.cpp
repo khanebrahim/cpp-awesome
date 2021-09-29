cfcc0525e8f7710c51214bb0f8587956b20fb20a
BengalDroid <bengaldroid@gmail.com>
2021-09-29 17:46:47 +0600

    array

---
+++ arrray_1.cpp
@@ -0,0 +1,75 @@
+/*#include <iostream>
+using namespace std;
+int main()
+{
+  int A[5] = {3, 4, 7, 11, 23};
+  for (int i = 0; i < 5; i++)
+  {
+    cout << A[i] << endl;
+  }
+  return 0;
+}
+*/
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+  float A[5] = {3.5, 4.5, 7.7, 11.2, 23.9};
+  for (float X : A)
+  {
+    cout << X << endl;
+  }
+  return 0;
+}
+*/
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+  float A[5] = {3.5, 4.5, 7.7, 11.2, 23.9};
+  for (int X : A)
+  {
+    cout << X << endl; //display int number
+  }
+  return 0;
+}
+*/
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+  float A[5] = {3.5f, 4.5f, 7.7f, 11, 23};
+  for (float X : A)
+  {
+    cout << X << endl;
+  }
+  return 0;
+}
+*/
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+  float A[5] = {3.5, 4.5, 7.7, 11.2, 23.9};
+  for (float X : A)
+  {
+    cout << X << endl; //display float number
+  }
+  return 0;
+}
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+  double A[5] = {3.4, 4.5, 7.2, 11.6, 23.3};
+  for (double X : A)
+  {
+    cout << X << endl;
+  }
+  return 0;
+}
