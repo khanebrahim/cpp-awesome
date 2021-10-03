69e56aa0fc30a706659515d6432884de6f57f94e
BengalDroid <bengaldroid@gmail.com>
2021-10-03 12:42:55 +0600

    sum matrix

--- 2d_array_sum.cpp
+++ 2d_array_sum.cpp
@@ -4,12 +4,20 @@ int main()
 {
   int A[2][3] = {2, 4, 6, 3, 7, 9};
   int B[2][3] = {9, 4, 6, 5, 7, 9};
-  int c[2][3];
+  int C[2][3];
   for (int i = 0; i < 2; i++)
   {
     for (int j = 0; j < 3; j++)
     {
-      cout << c[i][j] << " ";
+      C[i][j] = A[i][j] + B[i][j];
+    }
+    cout << endl;
+  }
+  for (int i = 0; i < 2; i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      cout << C[i][j] << " ";
     }
     cout << endl;
   }
--- 2d_for_each_loop.cpp
+++ 2d_for_each_loop.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-  int A[2][3];
+  int A[2][3] = {2, 4, 5, 6, 8, 9};
   for (auto &x : A)
   {
     for (auto &y : x)
