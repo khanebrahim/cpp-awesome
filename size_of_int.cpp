02a7d833bad400c2f67db4f67e2a68340420af69
BengalDroid <bengaldroid@gmail.com>
2021-10-04 18:51:48 +0600

    size of int

---
+++ size_of_int.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int size;
+  cout << "Enter the size of elements: ";
+  cin >> size;
+  int A[size];
+  cout << sizeof A << endl;
+  return 0;
+}
