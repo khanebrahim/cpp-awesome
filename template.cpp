f1a1bd385c77973fd7f27e365f7c5f51b1536dd3
BengalDroid <bengaldroid@gmail.com>
2021-10-07 11:52:41 +0600

    template

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+template <class T>
+T maxim(T a, T b)
+{
+  return a > b ? a : b;
+}
+int main()
+{
+  cout << maxim(12, 14) << endl;
+  cout << maxim(13.8, 12.5) << endl;
+  return 0;
+}
