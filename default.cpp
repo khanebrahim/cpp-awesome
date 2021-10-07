fb543f07b8bc8499d75fabcec93e01d01f8001ff
BengalDroid <bengaldroid@gmail.com>
2021-10-07 13:10:00 +0600

    default arguments

--- default.cpp
+++ default.cpp
@@ -1,4 +1,5 @@
 //default arguments
+/*
 #include <iostream>
 using namespace std;
 int sum(int a, int b, int c = 0)
@@ -9,5 +10,18 @@ int main()
 {
   cout << sum(12, 31) << endl;
   cout << sum(12, 31, 42) << endl;
+  return 0;
+}
+*/
+
+#include <iostream>
+using namespace std;
+int max(int a, int b, int c = 0)
+{
+  return a > b && a > c ? a : (b > c ? b : c);
+}
+int main()
+{
+  cout << max(17, 12, 15) << endl;
   return 0;
 }
