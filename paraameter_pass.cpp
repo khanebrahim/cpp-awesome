ced49216c9567f2e362f92527e08ccb9d065756e
BengalDroid <bengaldroid@gmail.com>
2021-10-08 11:36:15 +0600

    pass by address and refference

---
+++ paraameter_pass.cpp
@@ -0,0 +1,36 @@
+// Pass by Address and reference
+/*
+#include <iostream>
+using namespace std;
+void swap(int a, int b)
+{
+  int temp;
+  temp = a;
+  a = b;
+  b = temp;
+  cout << a << " " << b << endl;
+}
+int main()
+{
+  int x = 10, y = 20;
+  swap(x, y);
+}
+*/
+
+#include <iostream>
+using namespace std;
+void swap(int &a, int &b)
+{
+  cout << &a << " " << &b << endl;
+  int temp;
+  temp = a;
+  a = b;
+  b = temp;
+}
+int main()
+{
+  int x = 10, y = 20;
+  swap(x, y);
+  cout << &x << " " << &y << endl;
+  cout << x << " " << y << endl;
+}
