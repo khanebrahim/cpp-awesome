917c92a6001db8a4a758ec38f669e4ef4341a2c7
BengalDroid <bengaldroid@gmail.com>
2021-10-08 17:07:12 +0600

    return by address

--- paraameter_pass.cpp
+++ paraameter_pass.cpp
@@ -17,6 +17,7 @@ int main()
 }
 */
 
+/*
 #include <iostream>
 using namespace std;
 void swap(int &a, int &b)
@@ -33,4 +34,28 @@ int main()
   swap(x, y);
   cout << &x << " " << &y << endl;
   cout << x << " " << y << endl;
+}
+*/
+// return by address
+
+#include <iostream>
+using namespace std;
+int *fun()
+{
+  int *p = new int[5];
+  for (int i = 1; i <= 5; i++)
+  {
+    p[i] = 5 * i;
+  }
+  return p;
+}
+
+int main()
+{
+  int *q = fun();
+  for (int i = 1; i <= 5; i++)
+  {
+    cout << q[i] << endl;
+  }
+  return 0;
 }
